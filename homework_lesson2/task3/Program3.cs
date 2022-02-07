1e01f44501498f16ba3151e33a453a557c593696
dr_shov <dr_shov@rambler.ru>
2022-02-07 18:39:09 +0300

    Program3 code is complited

---
+++ homework_lesson2/task3/Program3.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+Console.WriteLine("Введите любое число: ");
+
+string Str1 = Console.ReadLine();
+int a = Str1.Length;
+
+if (3 > a)
+{
+    Console.WriteLine("В предложенном числе третья цифра отсутствует!");
+}
+else
+{
+    Console.Write(Str1[2]);
+    Console.Write("- третья цифра предложенного числа");
+}
