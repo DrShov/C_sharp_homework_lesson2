be56c468c24257ca11fda1f05793e1a29924b541
dr_shov <dr_shov@rambler.ru>
2022-02-07 18:34:12 +0300

    program1 code is complited

---
+++ homework_lesson2/task1/Program1.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите любое трехзначное число: ");
+
+string str1 = Console.ReadLine();
+
+Console.Write("Вторая цифра в вашем числе:");
+Console.Write(str1[1]);
