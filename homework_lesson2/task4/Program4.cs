245184436372748975d95388b8cfa9a5fe89e4a2
dr_shov <dr_shov@rambler.ru>
2022-02-07 19:37:10 +0300

    Program4 code is complited

---
+++ homework_lesson2/task4/Program4.cs
@@ -0,0 +1,18 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+
+Console.WriteLine("Введите интресующий Вас день в формате 'год, месяц, день': ");
+
+string str1 = Console.ReadLine();
+
+DateOnly SomeDate = DateOnly.Parse(str1);
+
+if (SomeDate.DayOfWeek == DayOfWeek.Sunday || SomeDate.DayOfWeek == DayOfWeek.Saturday)
+{
+    Console.WriteLine("Ура! Выходной! Отдыхайте!");
+}
+else
+{
+    Console.WriteLine("Как жаль! Будний день! Идите работать!");
+}
+
+
