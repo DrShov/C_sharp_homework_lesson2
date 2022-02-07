8179a308fbfe13069b741eeb53aaae0fbe0af82f
dr_shov <dr_shov@rambler.ru>
2022-02-07 18:36:46 +0300

    Program2 code is complited

---
+++ homework_lesson2/task2/Program2.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите любое трехзначное число: ");
+
+string Str1 = Console.ReadLine();
+
+Console.Write("Редактирование в объеме удаления второй цифры произведено: ");
+Console.Write(Str1.Remove(1, 1));
